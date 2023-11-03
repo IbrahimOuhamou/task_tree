224195c8573fef0cd5f9a86647f111d25acd256c
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-11-03 18:15:47 +0100

    BismiAllah

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "../include/task_t.h"
 #include "../include/tlist.h"
 
@@ -12,22 +13,6 @@ int main()
     printf("in the name of Allah\n");
     struct tlist_t tlist;
     tlist_init(&tlist);
-
-    srand(time(NULL));
-
-    for(int i = 0; i < 100; i++)
-    {
-        tlist_add_task(&tlist, newtask(names_list[rand() % 5]));
-	tlist.data[i]->progress = rand() % 101;
-    }
-
-    for (int i = 0; i < tlist.size; i++)
-    {
-        printf("tlist[%d]: \n", i);
-	printf("    id = %d\n", tlist.data[i]->id);
-	printf("    name = %s\n", tlist.data[i]->name);
-	printf("    progress = %u\n", tlist.data[i]->progress);
-    }
 
     //printf("task id: %d\ntask name %s\n", tlist.data[0]->id, tlist.data[0]->name);
 
