91b9a69eddccce0e4a0765ca5b1e1710304a0e18
Ibrahim Ouhamou <ouhamouy10@gmail.com>
2023-12-15 23:17:55 +0100

    BismiAllah, made some progress by the will of Allah

--- src/task_tree_ui.c
+++ src/task_tree_ui.c
@@ -22,15 +22,23 @@ GtkWidget* task_tree_ui_task_widget(struct task_t* task)
     GtkWidget* frame = gtk_frame_new(task->name);
     GtkWidget* grid = gtk_grid_new();
 
-    GtkWidget* label = gtk_label_new("name: ");
-    gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
-    GtkWidget* name_buff = gtk_entry_new();
-    gtk_grid_attach(GTK_GRID(grid), name_buff, 1, 0, 1, 1);
-    GtkWidget* button = gtk_button_new_with_label("change name");
-    gtk_grid_attach(GTK_GRID(grid), button, 2, 0, 1, 1);
-    g_signal_connect(button, "clicked", G_CALLBACK(task_tree_ui_task_set_name), name_buff);
-
-    g_print("task's name: %s\n", task->name);
+    {//name (row=0)
+        GtkWidget* label = gtk_label_new("name: ");
+        gtk_grid_attach(GTK_GRID(grid), label, 0, 0, 1, 1);
+        GtkWidget* name_buff = gtk_entry_new();
+        gtk_grid_attach(GTK_GRID(grid), name_buff, 1, 0, 1, 1);
+        GtkWidget* button = gtk_button_new_with_label("change name");
+        gtk_grid_attach(GTK_GRID(grid), button, 2, 0, 1, 1);
+        g_signal_connect(button, "clicked", G_CALLBACK(task_tree_ui_task_set_name), name_buff);
+    }
+    {//progress (row=1)
+        GtkWidget* label = gtk_label_new("progress :");
+        gtk_grid_attach(GTK_GRID(grid), label, 0, 1, 1, 1);
+        //GtkWidget* scale = gtk_scale_new(GTK_ORIENTATION_HORIZONTAL, NULL);
+        GtkWidget* scale = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 100, 1);
+        gtk_scale_set_value_pos(GTK_SCALE(scale), GTK_POS_LEFT);
+        gtk_grid_attach(GTK_GRID(grid), scale, 1, 1, 2, 1);
+    }
     gtk_frame_set_child(GTK_FRAME(frame), grid);
     return frame;
 }
