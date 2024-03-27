118287202fc022c3533036db1d52914ab0860542
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-27 18:11:35 +0000

    alhamdo li Allah

---
+++ src/bismi_allah_nk.c
@@ -0,0 +1,18 @@
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    printf("in the name of Allah\n");
+    printf("la ilaha illa Allah mohammed rassoul Allah\n");
+
+
+
+
+}
+
+
